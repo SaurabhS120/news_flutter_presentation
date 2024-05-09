c7e82e9865407da5f14fcf67f6442a41d10f7f01
SaurabhS120 <saurabh.sonar120@gmail.com>
2024-05-09 09:29:24 +0530

    Test group for news entity testing

--- test/home_page_test.dart
+++ test/home_page_test.dart
@@ -1,4 +1,5 @@
 import 'package:either_dart/either.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
@@ -37,7 +38,7 @@ void main() async {
     verify(newsRepo.getNews()).called(1);
   });
 
-  test("News entity parse test", () {
+  group("News entity parse test", () {
     Map<String, dynamic> response = {
       "status": "ok",
       "totalResults": 9154,
@@ -71,16 +72,54 @@ void main() async {
           "content":
               "Elon Musks artificial intelligence startup X.AI Corp. is set to close its funding round at a valuation of about \$18 billion as soon as this week, according to people familiar with the matter.The size… [+1413 chars]"
         },
+        {
+          "source": {"id": null, "name": "Ducttapemarketing.com"},
+          "author": "John Jantsch",
+          "title":
+              "How to Win The Consumer Boredom-Span: The Key to Cutting Through Clutter",
+          "description":
+              "How to Win The Consumer Boredom-Span: The Key to Cutting Through Clutter written by John Jantsch read more at Duct Tape Marketing\nThe Duct Tape Marketing Podcast with John Jantsch In this episode of the Duct Tape Marketing Podcast, I interviewed Alan Dibb. Th…",
+          "url":
+              "https://ducttapemarketing.com/win-consumer-boredom-span-lean-marketing/",
+          "urlToImage":
+              "https://i0.wp.com/ducttapemarketing.com/wp-content/uploads/2024/05/magnet-me-LDcC7aCWVlo-unsplash-scaled.jpg?fit=2558%2C1706&ssl=1",
+          "publishedAt": "2024-05-09T01:53:50Z",
+          "content":
+              "Speaker 1 (00:00): I was like, I found it. I found it. This is what I've been looking for. I can honestly say it has genuinely changed the way I run my business. It's changed the results that I'm see… [+22550 chars]"
+        },
       ],
     };
     NewsResponseEntity responseEntity = NewsResponseEntity.fromJson(response);
-    expect(responseEntity.articles.first.title,
-        "Chinese EV maker Nio to launch its lower-priced brand Onvo on May 15");
-    expect(
-        responseEntity.articles.first.title, response['articles'][0]['title']);
-    expect(responseEntity.articles[1].urlToImage,
-        "https://img.etimg.com/thumb/msid-109963888,width-1200,height-630,imgsize-93376,overlay-ettech/photo.jpg");
-    expect(responseEntity.articles[1].urlToImage,
-        response['articles'][1]['urlToImage']);
+    String? expected = "";
+
+    test("News title parsing test", () {
+      expect(responseEntity.articles.first.title,
+          "Chinese EV maker Nio to launch its lower-priced brand Onvo on May 15");
+      expected = response['articles'][0]['title'];
+      debugPrint("title : $expected");
+      expect(responseEntity.articles.first.title, expected);
+      expected = response['articles'][1]['title'];
+      debugPrint("title : $expected");
+      expect(responseEntity.articles[1].title, expected);
+      expected = response['articles'][2]['title'];
+      debugPrint("title : $expected");
+      expect(responseEntity.articles[2].title, expected);
+    });
+
+    test("News image url parse test", () {
+      expected = response['articles'][0]['urlToImage'];
+      debugPrint("imageUrl : $expected");
+      expect(responseEntity.articles[0].urlToImage, expected);
+
+      expected = response['articles'][1]['urlToImage'];
+      debugPrint("imageUrl : $expected");
+      expect(responseEntity.articles[1].urlToImage,
+          "https://img.etimg.com/thumb/msid-109963888,width-1200,height-630,imgsize-93376,overlay-ettech/photo.jpg");
+      expect(responseEntity.articles[1].urlToImage, expected);
+
+      expected = response['articles'][2]['urlToImage'];
+      debugPrint("imageUrl : $expected");
+      expect(responseEntity.articles[2].urlToImage, expected);
+    });
   });
 }
